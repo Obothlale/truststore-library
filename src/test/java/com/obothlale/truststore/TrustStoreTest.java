9c62f03fffebf4de3e96092b0dd49cd755466c31
obothlalekganane <elalhtobo@gmail.com>
2019-06-30 13:20:34 +0200

    refactor tests

--- src/test/java/com/obothlale/truststore/TrustStoreTest.java
+++ src/test/java/com/obothlale/truststore/TrustStoreTest.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.security.cert.Certificate;
 import java.util.List;
 
@@ -18,39 +16,27 @@ public class TrustStoreTest {
     @Before
     public void setup() {
         trustStore = new TrustStoreImpl();
+        String trustStoreFile = "truststore.jks";
+        trustStore.create("src/main/resources/" + trustStoreFile);
     }
 
     @Test
-    public void testCreate() {
+    public void testCreateTrustStore() {
         String trustStoreFile = "truststore.jks";
-        trustStore.create("src/main/resources/" + trustStoreFile);
         File file = new File(getClass().getClassLoader().getResource(trustStoreFile).getFile());
         Assert.assertNotNull(file);
     }
 
     @Test
-    public void testSaveCertificates() throws Exception {
+    public void testSaveCertificatesInTrustStore() {
         String trustStoreFile = "truststore.jks";
-        byte[] previousFileBytes = getFileInBytes(trustStoreFile);
-
         String certificateFile = "BAGL Root CA 2.cer";
-        String bag_root = "bag_root";
-        trustStore.save("src/main/resources/" + certificateFile, bag_root, "src/main/resources/" + trustStoreFile);
+        String alias = "bag_root";
+        List<Certificate> certificates = trustStore.getAllCertificates("src/main/resources/" + trustStoreFile);
 
-        byte[] afterSaveFileBytes = getFileInBytes(trustStoreFile);
-        Assert.assertNotEquals(previousFileBytes.length, afterSaveFileBytes.length);
+        Assert.assertFalse(certificates.size() > 0);
+        trustStore.save("src/main/resources/" + certificateFile, alias, "src/main/resources/" + trustStoreFile);
+        certificates = trustStore.getAllCertificates("src/main/resources/" + trustStoreFile);
+        Assert.assertTrue(certificates.size() > 0);
     }
-
-    private byte[] getFileInBytes(String trustStoreFile) throws IOException {
-        return Files.readAllBytes((new File(getClass().getClassLoader().getResource(trustStoreFile).getFile())).toPath());
-    }
-
-    @Test
-    public void testGetCerts() throws IOException {
-        String trustStoreFile = "truststore.jks";
-        List<Certificate> allCertificates = trustStore.getAllCertificates("src/main/resources/" + trustStoreFile);
-        Assert.assertTrue(allCertificates.size() > 0);
-        System.out.println(allCertificates);
-    }
-
 }
